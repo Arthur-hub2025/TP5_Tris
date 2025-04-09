665d82c6c854133ed4643fb740dfa3cd11a70694
Arthur PAILLE <a.paille@lyceesaintefamille.com>
2025-04-09 13:00:03 +0200

    fin exercice 1

--- TPTris.c
+++ TPTris.c
@@ -1,12 +1,12 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char nom[50];
     char code[20];
-    char fabri[11];
-    char peremption[11];
+    char fabri[11];  
+    char peremption[11];  
     float prix;
     int vente;
     int stock;
@@ -74,21 +74,18 @@ int main() {
 
         if (cmp == 0) {
             trouve = 1;
-        }
-        else if (cmp < 0) {
+        } else if (cmp < 0) {
             debut = mil + 1;
-        }
-        else {
+        } else {
             fin = mil - 1;
         }
     }
 
     if (trouve) {
         printf("\n Medicament 'paracetamol' trouve a l'indice %d :\n", mil);
-        printf("Nom : %s | Code : %s | Peremption : %s | Prix : %.2f$\n",
+        printf("Nom : %s | Code : %s | Peremption : %s | Prix : %.2f$\n", 
             tab[mil].nom, tab[mil].code, tab[mil].peremption, tab[mil].prix);
-    }
-    else {
+    } else {
         printf("\n Le medicament 'paracetamol' n'est pas present dans le tableau.\n");
     }
 
@@ -101,7 +98,7 @@ int main() {
 
     printf("\n Medicament le plus cher :\n");
     printf("Nom : %s | Code : %s | Prix : %.2f$ | Peremption : %s | Stock : %d\n",
-        tab[indice_max].nom, tab[indice_max].code, tab[indice_max].prix, tab[indice_max].peremption, tab[indice_max].stock);
+           tab[indice_max].nom, tab[indice_max].code, tab[indice_max].prix, tab[indice_max].peremption, tab[indice_max].stock);
 
     int total_vendus = 0;
     int total_disponibles = 0;
@@ -117,6 +114,57 @@ int main() {
     }
 
     printf("\n Taux de medicaments vendus : %.2f %%\n", taux);
+
+    typedef struct Node {
+        Medicament med;
+        struct Node* suivant;
+    } Node;
+
+    Node* tete = NULL;
+    Node* courant;
+
+    for (int i = 0; i < N; i++) {
+        Node* nouveau = (Node*)malloc(sizeof(Node));
+        nouveau->med = tab[i];
+        nouveau->suivant = NULL;
+
+        if (tete == NULL) {
+            tete = nouveau;
+            courant = tete;
+        } else {
+            courant->suivant = nouveau;
+            courant = courant->suivant;
+        }
+    }
+
+    int trie;
+    do {
+        trie = 1;
+        Node* p = tete;
+        while (p != NULL && p->suivant != NULL) {
+            if (strcmp(p->med.code, p->suivant->med.code) > 0) {
+                Medicament tmp = p->med;
+                p->med = p->suivant->med;
+                p->suivant->med = tmp;
+                trie = 0;
+            }
+            p = p->suivant;
+        }
+    } while (!trie);
+
+    printf("\nListe chainee triee par code de medicament :\n");
+    courant = tete;
+    while (courant != NULL) {
+        printf("Nom : %s | Code : %s | Prix : %.2f$\n",
+               courant->med.nom, courant->med.code, courant->med.prix);
+        courant = courant->suivant;
+    }
+
+    while (tete != NULL) {
+        Node* tmp = tete;
+        tete = tete->suivant;
+        free(tmp);
+    }
 
     return 0;
 }
